d9447879f6d38f1c6671e737e18d21a27f7ecb5e
Jose Allan Kardec Goncalves <joseallan@gmail.com>
2023-05-28 10:15:54 -0300

    alula 18 - try catch

---
+++ aula-18.js
@@ -0,0 +1,15 @@
+
+
+function calcular(valor){
+    if(valor === 0){
+        throw new Error('Não pode ser 0');
+    }
+    return valor / 2;
+}
+
+try{
+    let resultado = calcular(50);
+    console.log('resultado :', resultado);
+}catch (error){
+    console.log(error.message);
+}
