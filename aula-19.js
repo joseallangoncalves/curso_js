a1272aa69b059daf05271abac9e6e51c48441005
Jose Allan Kardec Goncalves <joseallan@gmail.com>
2023-05-28 10:41:24 -0300

    aula 19 - formas diferentes de fazer um função

---
+++ aula-19.js
@@ -0,0 +1,16 @@
+//formas diferentes de fazer um função
+
+function calcular(){
+    console.log('calcular');
+}
+
+const calcular2 = function(){
+    console.log('calcular 2');
+}
+
+const calcular3 = () => console.log('calcular3');
+
+
+calcular();
+calcular2();
+calcular3();
